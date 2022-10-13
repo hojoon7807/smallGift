2eafc06ee3fb754949d8f813a79f51a4fd8d1b4c
hojoon <ujuj0202@gmail.com>
2022-10-13 14:49:37 +0900

    fix: s3 사용 불가해 config gradle 임시 주석 처리

--- src/main/java/com/sgwannabig/smallgift/springboot/config/S3Config.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/config/S3Config.java
@@ -11,26 +11,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class S3Config {
 
-  @Value("${cloud.aws.credentials.access-key}")
-  private String accessKey;
-
-  @Value("${cloud.aws.credentials.secret-key}")
-  private String secretKey;
-
-  @Value("${cloud.aws.region.static}")
-  private String region;
-
-  @Bean
-  public BasicAWSCredentials basicAWSCredentials() {
-    return new BasicAWSCredentials(accessKey, secretKey);
-  }
-
-  @Bean
-  public AmazonS3 amazonS3Client() {
-    return AmazonS3ClientBuilder
-        .standard()
-        .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials()))
-        .withRegion(region)
-        .build();
-  }
+//  @Value("${cloud.aws.credentials.access-key}")
+//  private String accessKey;
+//
+//  @Value("${cloud.aws.credentials.secret-key}")
+//  private String secretKey;
+//
+//  @Value("${cloud.aws.region.static}")
+//  private String region;
+//
+//  @Bean
+//  public BasicAWSCredentials basicAWSCredentials() {
+//    return new BasicAWSCredentials(accessKey, secretKey);
+//  }
+//
+//  @Bean
+//  public AmazonS3 amazonS3Client() {
+//    return AmazonS3ClientBuilder
+//        .standard()
+//        .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials()))
+//        .withRegion(region)
+//        .build();
+//  }
 }
