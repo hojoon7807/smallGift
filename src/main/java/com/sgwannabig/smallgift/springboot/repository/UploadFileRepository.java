4e8cce194b0e8e18ddc2e6eb69bf9ed066b92725
hojoon <ujuj0202@gmail.com>
2022-10-14 23:47:14 +0900

    chore: 사용 못하는 class 우선 주석처리

--- src/main/java/com/sgwannabig/smallgift/springboot/repository/UploadFileRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.sgwannabig.smallgift.springboot.repository;
-//
-//import com.sgwannabig.smallgift.springboot.domain.file.UploadFile;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
-//}
